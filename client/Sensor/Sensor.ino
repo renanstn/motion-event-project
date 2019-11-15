1a212cfc827cd330b6ea6e5d7aa8773a2ef50fcb
Doc McCoy <renan_ota@hotmail.com>
2019-11-14 22:36:43 -0300

    Upload teste sensor

---
+++ client/Sensor/Sensor.ino
@@ -0,0 +1,26 @@
+const int trigPin = 2;
+const int echoPin = 4;
+
+long duration;
+int distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+
+  distance = duration * 0.034/2;
+
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  delay(2000);
+}
